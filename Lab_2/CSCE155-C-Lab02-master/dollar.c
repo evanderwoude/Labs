d7aac4d31fda91cd4ef9c29c0e6af31d1167d0b3
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-03 17:03:29 -0500

    Create dollar.c

---
+++ Lab_2/CSCE155-C-Lab02-master/dollar.c
@@ -0,0 +1,18 @@
+/*
+Write a program that will convert US Dollars to British Pounds and Japanese JPY. 10%
+of the total amount of US Dollars will be taken as an exchange fee. For the rest of the
+US Dollars, half will be changed to British Pounds and the other half to JPY. Assume
+the exchange rate is: 1 US Dollar = 0.6 British Pound; 1 US Dollar = 76.8 JPY. The
+3
+program should ask the user to input the amount of US dollars then print an appropriate
+output. An example run would look something like the following:
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+double main (){
+	double USD, BP, JPY;
+
+	
+}
