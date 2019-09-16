6bb6d6d9b8a5d3a96eabe2cc12a2943c7d267b4a
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-16 13:44:36 -0500

    Create rss.c

---
+++ Lab_11/CSCE155-C-Lab11-master/rss.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "rss.h"
+
+// implement these functions
+
+Rss * createEmptyRss() {
+}
+
+Rss * createRss(const char * title, const char * link, const char * date, const char * description) {
+}
+
+void printRss(Rss * item) {
+}
