738e82f0e1c42d9a733dcdbfb7825100cbc758ff
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-16 18:05:44 -0500

    Update guessingGame.c

--- Lab_4/CSCE155-C-Lab04-master/guessingGame.c
+++ Lab_4/CSCE155-C-Lab04-master/guessingGame.c
@@ -8,10 +8,12 @@
  */
 #include<stdlib.h>
 #include<stdio.h>
+#include <time.h>
 
 int main(int argc, char **argv) {
 
   int n = 1000;
+  int i = 1;
 
   //seed the random number generator
   srand(time(NULL));
@@ -20,10 +22,44 @@ int main(int argc, char **argv) {
   int guess = -10;
   int num_guesses = 0;
 
+  int upper = n;
+  int lower = 0;
+
   printf("Guess-A-Number Game!\n");
+  printf("If you get it wrong it will tell you which half the number is on\n");
   printf("Enter a number between 1 and %d\n", n);
 
-  //TODO: place your code here
+  while(i == 1)
+  {
+    scanf("%d", &guess); //take in input
+
+    if (guess == number){ //check if number is right if not give a hint
+      i--;
+    }else if(guess < lower || guess > upper){
+      printf("That's outside the range... check your math \n");
+    }else{
+      if(guess < number){ //if lower than number higher
+        printf("Guess higher, between ");
+        lower = guess;
+        if(number < (upper - (upper-lower)/2)){ // if the number is less than half way between current limit; lower upper
+          upper = (upper - ((upper-lower)/2));
+        }else{                          // if the number is greater than half way between current limit; rasie lower
+          lower = (lower + ((upper-lower)/2));
+        }
+        printf("%d and %d\n", lower, upper);
+      }else if(guess > number){ //if higher than number guess lower
+        printf("Guess lower, between ");
+        upper = guess;
+        if(number < (upper - (upper-lower)/2)){ // if the number is less than half way between current limit; lower upper
+          upper = (upper - ((upper-lower)/2));
+        }else{                          // if the number is greater than half way between current limit; rasie lower
+          lower = (lower + ((upper-lower)/2));
+        }
+        printf("%d and %d\n", lower, upper);
+      }
+    }
+    num_guesses++;
+  }
 
   printf("Congratulations, you found it!  Number of guesses: %d\n", num_guesses);
   return 0;
