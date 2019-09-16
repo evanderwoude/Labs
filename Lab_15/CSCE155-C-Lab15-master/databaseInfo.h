4046ae3fe8f9cf9791cfca811fa06f16316dc9c5
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-16 13:45:48 -0500

    Create databaseInfo.h

---
+++ Lab_15/CSCE155-C-Lab15-master/databaseInfo.h
@@ -0,0 +1,18 @@
+
+// The name of the driver to be used on the system
+const char* driver = "libmaodbc";
+
+// Server the database is stored on
+const char* server = "cse.unl.edu";
+
+// Port for the server
+const char* port = "3306";
+
+// Name of the database on the server
+const char* database = "";
+
+// Username for the database
+const char* userID = "";
+
+// Password for previous user name
+const char* password = "";
