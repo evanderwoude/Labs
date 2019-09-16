ec3563343eae114eb17d40da90c00bbdce531fb9
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-16 18:05:47 -0500

    Update primes.c

--- Lab_4/CSCE155-C-Lab04-master/primes.c
+++ Lab_4/CSCE155-C-Lab04-master/primes.c
@@ -14,13 +14,23 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  int n = atoi(argv[1]);
+  int n = atof(argv[1]);
   int i;
 
   for(i=2; i<=n; i++) {
     int isPrime = 1;
-    //TODO: write a loop that tests whether or not
-    //      the integer i is prime
+
+    for(int j = 2; j <= (i/2); j++){ //from 2 up till the numbers square
+      if (i == 4){ //4 is tricky because 2 doesn't work in equation
+        isPrime = 0; //false
+        break;  //jump out for that number
+      }else if(i%j == 0 && i != 2){ //if the number can be divied by another number, up to but not including it's square, it's not prime
+        isPrime = 0; //not prime
+        break;  //jump out for that number
+      }else{    //if number can't be divided then it's prime
+        isPrime = 1; //true
+      }
+    }
 
     if(isPrime) {
       printf("%d is prime\n", i);
