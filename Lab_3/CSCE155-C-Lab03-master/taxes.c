e3306a83bb0ceb0377ce0219cbaed8c81196621a
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-16 13:46:31 -0500

    Update taxes.c

--- Lab_3/CSCE155-C-Lab03-master/taxes.c
+++ Lab_3/CSCE155-C-Lab03-master/taxes.c
@@ -27,8 +27,36 @@ int main(int argc, char **argv) {
     scanf("%d", &numChildren);
   }
 
-  //TODO: compute the tax, child credit, and total tax:
+  childCredit = numChildren*1000;
 
+  //TODO: compute the tax, child credit, and total tax:
+  if (agi >= 0 && agi <= 17000)
+  {
+    tax = .1;
+    tax = (tax*agi);
+  }else if (agi >= 17000 && agi <= 69000)
+  {
+    tax = .15;
+    tax = (tax*(agi-17000)+1700);
+  }else if (agi >= 69000 && agi <= 139350)
+  {
+    tax = .25;
+    tax = (tax*(agi-69000)+9500);
+  }else if (agi >= 139350 && agi <= 212300)
+  {
+    tax = .28;
+    tax = (tax*(agi-139350)+27087.50);
+  }else if (agi >= 212300 && agi <= 379150)
+  {
+    tax = .33;
+    tax = (tax*(agi-212300)+47513.50);
+  }else if (agi >= 379150)
+  {
+    tax = .35;
+    tax = (tax*(agi-379150)+102574.00);
+  }
+
+  totalTax = tax - childCredit;
 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
