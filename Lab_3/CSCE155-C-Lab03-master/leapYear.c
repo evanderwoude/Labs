1e3a24979cfeb9d672e15d0097e32a4eedf46c92
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-16 13:46:33 -0500

    Update leapYear.c

--- Lab_3/CSCE155-C-Lab03-master/leapYear.c
+++ Lab_3/CSCE155-C-Lab03-master/leapYear.c
@@ -1,3 +1,8 @@
+/*
+ * Eric Vander Woude and Ireland Gaffey
+ * Date 9/10/2019
+ */
+
 /**
  * This program determines if various years are leap years or not.
  *
@@ -51,6 +56,36 @@ int main(int argc, char **argv) {
     numPassed = numPassed + 1;
   }
 
+  year = 1000;
+  printf("Test Case 1: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  year = 1001;
+  printf("Test Case 2: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  year = 1100;
+  printf("Test Case 3: year = %d: ", year);
+  if(isLeapYear(year)) {
+    printf("FAILED!\n");
+    numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
   //TODO: write at least 3 more of your own test cases
 
   printf("\n\n");
@@ -63,8 +98,19 @@ int main(int argc, char **argv) {
 }
 
 int isLeapYear(int year) {
-  //TODO: Write your logic here
-  //      The year is stored in the variable year
-  //      Your function should return true (1) if it represents a leap year
-  //      and false (0) if it does not.
+
+  if (year%4 == 0){
+    if (year%100 == 0){
+      if (year%400 == 0){
+        return 1;
+      }else{
+        return 0;
+      }
+    }else{
+      return 1;
+    }
+  }
+  else{
+    return 0;
+  }
 }
