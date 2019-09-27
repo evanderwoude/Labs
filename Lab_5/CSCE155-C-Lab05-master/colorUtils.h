6fcdc2ec166fbc281f3e3fa4075152b9c7c79191
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-27 18:27:44 -0500

    Create colorUtils.h

---
+++ Lab_5/CSCE155-C-Lab05-master/colorUtils.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int toGrayScaleAverage(int r, int g, int b);
+
+int max(int r, int g, int b);
+
+int min(int r, int g, int b);
+
+int toGrayScaleLightness(int r, int g, int b);
+
+int toGrayScaleLuminosity(int r, int g, int b);
+
+int toSepiaRed(int r, int g, int b);
+
+int toSepiaGreen(int r, int g, int b);
+
+int toSepiaBlue(int r, int g, int b);
