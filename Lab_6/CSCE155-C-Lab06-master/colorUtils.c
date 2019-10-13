e61180a6e6de82bbfc962ff2a45f5c4a8fca5243
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-10-13 18:11:01 -0500

    Update colorUtils.c

--- Lab_6/CSCE155-C-Lab06-master/colorUtils.c
+++ Lab_6/CSCE155-C-Lab06-master/colorUtils.c
@@ -12,9 +12,26 @@ int min(int a, int b, int c) {
 }
 
 int toGrayScale(int *r, int *g, int *b, Mode mode) {
-  //TODO: implement
+  if(r == NULL || g == NULL || b == NULL){
+    return ERROR_NULL;
+  } else if(r < 0 || g < 0 || b < 0){
+    return ERROR_NEGATIVE;
+  } else if(mode < 0 || mode > 2 ){
+    return NO_MODE;
+  } else{
+    if(mode == 0){
+      *r = *g = *b = ((*r + *g + *b)/3);
+    }else if(mode == 1){
+      *r = *g = *b = ((max(*r, *g, *b) + min(*r, *g, *b))/2);
+    }else if(mode == 2){
+      *r = *g = *b = (.21*(*r) + .72*(*g) + .07*(*b));
+    }
+    return NO_ERROR;
+  }
 }
 
 int toSepia(int *r, int *g, int *b) {
-  //TODO: implement
+	*r = 0.393*(*r) + .769*(*g) + .189*(*b);
+  *g = 0.349*(*r) + .686*(*g) + .168*(*b);
+  *b = 0.272*(*r) + .534*(*g) + .131*(*b);
 }
